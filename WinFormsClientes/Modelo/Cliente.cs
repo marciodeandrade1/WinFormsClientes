c7051cf2214ace19faf9050c6e9259a6a05ed4eb
Marcio de Andrade <marcio@requestdev.net>
2025-02-22 07:59:21 -0400

    Add project files.

---
+++ WinFormsClientes/Modelo/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsClientes.Modelo
+{
+    public class Cliente
+    {
+        // construtor
+        public Cliente() { }
+
+        // Propriedades - Private
+        private int id;
+        private string nome;
+        private string email;
+
+        // Propriedades - Public
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+}
